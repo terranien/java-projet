b64c97fb16d397b49199bf0fc01a3c38b901863e
Maxime Voets <maximevoets>
2012-12-16 23:38:23 +0100

    ajout nouvelles méthodes + variables

--- src/Classes/Mots.java
+++ src/Classes/Mots.java
@@ -1,14 +1,23 @@
 package Classes;
 
+import java.io.Console;
 import java.util.ArrayList;
 
 public class Mots  {
 //private int nbalea;
-private String[] tabmot = null ;
-private int compte_point;
-private boolean trouve ;
-private char tableau[];
-private char lettreJoueur;
+private String tabmot = null ; //Mot rentré par joueur
+private char tableau[]; // tableau pour le mot
+private char lettreJoueur; // lettre rentrée par joueur2
+private int compte_point; //total des points
+private int nbErreur = 8; // nombre d'erreurs possible
+private int longueurMot=tabmot.length(); // longueur du mot choisi
+private boolean trouve ; // devient vrai si mot trouvé
+private boolean correct = false; // vérifié si lettre entrée est correcte
+private boolean memeLettre = false; // vérifié si la lettre entrée est déjà rentrée et correcte
+private boolean memeLettre2 = false; // vérifié si la lettre entrée est déjà entrée et fausse 
+
+private char tabVerif[];
+tabVerif = new char [nbErreur]; // Tableau des lettres entrées par joueur
 
 public void cretab(){
 	tableau = tabmot.toCharArray(); 
@@ -16,11 +25,11 @@ public void cretab(){
 }
 
 
-public String[] getTabmot() {
+public String getTabmot() {
 	return tabmot;
 }
 
-public void setTabmot(String[] tabmot) {
+public void setTabmot(String tabmot) {
 	this.tabmot = tabmot;
 }
 
@@ -46,22 +55,52 @@ public void setTrouve(boolean trouve) {
 	this.trouve = trouve;
 }
 
-public int getNbalea() {
+/*public int getNbalea() {
 	return nbalea;
-}
+}*/
 
-public void setNbalea(int nbalea) {
+/*public void setNbalea(int nbalea) {
 	this.nbalea = nbalea;
-}
+}*/
 
-public int longueurMot(int len){
-	int len =tabmot.length();
-	return len; 
-}
+/*public int longueurMot(int len){
+	int len =tabmot.length;
+	return len;
+	Variable longueurMot déclarée en haut 
+}*/
+
+public boolean isCorrect(boolean memeLettre){
+	int i;
+	for (i=0;i<longueurMot;i++){
+		if (tableau[i]==lettreJoueur){
+			memeLettre = true;
+		}
+	}
+	return memeLettre;
+}//Boucle pour vérifié si la lettre entrée a déjà été validée dans le mot à trouver
+
+public boolean isCorrect2(boolean memeLettre2){
+	int i;
+	for (i=0;i<nbErreur;i++){ 
+        if (tabVerif[i]==lettreJoueur){ 
+            memeLettre2=true;
+        }
+    }
+	return memeLettre2;
+}//Boucle pour vérifié si la lettre entrée a déjà été entrée dans le tableau des lettres fausses tabVerif
 
 public void testMot(){
-	 
+	int i;
+	lettreJoueur=Console.readLine("Votre lettre? ").toLowerCase().charAt(0);
+		for(i=0;i<longueurMot;i++){
+			if (lettreJoueur==tableau.charAt(i)){
+			tableau[i]=lettreJoueur;
+			correct=true;
+		}
+	}	
 }
 
 
 }
+
+// Aide pour le code du site: http://www.javafr.com/codes/JEU-PENDU_35803.aspx
